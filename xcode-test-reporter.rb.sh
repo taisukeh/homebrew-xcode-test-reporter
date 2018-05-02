a319426c0ad6a83f20c17e9d836401c28ac55ee4
Taisuke Hori <taisuke.horix@gmail.com>
2018-05-02 20:41:06 +0900

    Fix

--- xcode-test-reporter.rb.sh
+++ xcode-test-reporter.rb.sh
@@ -1,10 +1,19 @@
+#!/usr/bin/env sh
+
+set -eu
+
+tag=`curl -s -L https://api.github.com/repos/taisukeh/xcode-test-reporter/releases/latest | jq -r '.tag_name'`
+
+sha256=`curl -s -L https://github.com/taisukeh/xcode-test-reporter/releases/download/$tag/xcode-test-reporter_darwin_x86_64.sha256`
+
+cat << EOL > xcode-test-reporter.rb
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 class XcodeTestReporter < Formula
   desc "Generates JUnit or HTML report from Xcode plist test report files."
   homepage "https://github.com/taisukeh/xcode-test-reporter"
-  url "https://github.com/taisukeh/xcode-test-reporter/releases/download/0.0.2/xcode-test-reporter_darwin_x86_64"
-  sha256 "1ee2d0d0465b0ae2fedbc44fea34aeb48f52493eeb21db12251f940befa57a3f"
+  url "https://github.com/taisukeh/xcode-test-reporter/releases/download/$tag/xcode-test-reporter_darwin_x86_64"
+  sha256 "$sha256"
 
   def install
     system "cp", "xcode-test-reporter_darwin_x86_64", "xcode-test-reporter_darwin"
@@ -16,3 +25,4 @@ class XcodeTestReporter < Formula
     system "#{bin}/xcode-test-reporter", "--help"
   end
 end
+EOL
