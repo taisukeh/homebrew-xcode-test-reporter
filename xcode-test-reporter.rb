2b93cf4e6597d8bafa9c9a0d4f07c51631691dd0
Taisuke Hori <taisuke.horix@gmail.com>
2018-05-04 02:40:03 +0900

    Fix

--- xcode-test-reporter.rb
+++ xcode-test-reporter.rb
@@ -3,8 +3,8 @@
 class XcodeTestReporter < Formula
   desc "Generates JUnit or HTML report from Xcode plist test report files."
   homepage "https://github.com/taisukeh/xcode-test-reporter"
-  url "https://github.com/taisukeh/xcode-test-reporter/releases/download/0.0.3/xcode-test-reporter_darwin_x86_64.tar.gz"
-  sha256 "2f9da2541677faf6f46853f9fe017313ffd69030cfc2ed19e545ae38cf36640b"
+  url "https://github.com/taisukeh/xcode-test-reporter/releases/download/0.0.5/xcode-test-reporter_darwin_x86_64.tar.gz"
+  sha256 "3d2694a290ead8d503e57011633cd410964bc02737957c17bace0e184830aba0"
 
   def install
     bin.install "xcode-test-reporter"
